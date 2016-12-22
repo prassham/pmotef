d12f088220541e2c716315cf4faeb07672a70475
Naga anusha Sabbineni <nsabbine@in.ibm.com>
2016-12-22 13:32:25 +0000

    made changes in landing.js

--- src/main/webapp/appjs/controller/landing.js
+++ src/main/webapp/appjs/controller/landing.js
@@ -20,23 +20,22 @@ $scope.user = PMOHttpService;
 	$scope.user.pending = true;
 	$scope.pendingbutton = $scope.user.pending;
 	
-	 $scope.loggedinuser = PMOHttpService
+ $scope.loggedinuser = PMOHttpService
 		.loginName().then(function (response) {
-			$scope.loggedinuser = response.data;
-			$scope.user.email = $scope.loggedinuser;
-			var e = $scope.user.email;
-			var id = "{\"email\""+":"+e +"}";
+			var id = JSON.stringify(response.data);
+			console.log(id);
 			
 			$scope.logindetails = PMOHttpService
 			.login(id).then(function (response) {
 				$scope.loggedindetails = response.data;
 				//alert($scope.loggedindetails[0].NAME);
 				$scope.user.name = $scope.loggedindetails[0].NAME;
-				
+				console.log($scope.user.name);
 				$scope.employeews_manager = PMOHttpService
 				.getEmployeeByName($scope.user.name).then(function(response) {
 					//alert(response.data)
 					$scope.user.pendingbutton = response.data;
+					console.log($scope.user.pendingbutton);
 					if($scope.user.pendingbutton == "exist"){
 					$scope.user.pending= false;
 					$scope.pendingbutton = $scope.user.pending;
