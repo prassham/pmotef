1d71fe7790715e5c4876b58577c043ef7eb4aa00
Naga anusha Sabbineni <nsabbine@in.ibm.com>
2016-12-30 15:48:19 +0000

    changed connection details in utilization

--- src/main/java/com/ibm/pmo/resources/PMOUtilizationHelperCloudant.java
+++ src/main/java/com/ibm/pmo/resources/PMOUtilizationHelperCloudant.java
@@ -125,11 +125,11 @@ public class PMOUtilizationHelperCloudant {
 	    url = url.replaceAll("^\"|\"$", "");
 	    System.out.println("connection establishment");
 		String uri = url +"/employee/_design/employeeDetails/_view/employeeDetails?reduce=true&group=true";
-		URL url = new URL(uri);
+		URL urlresource = new URL(uri);
 		String loginPassword = username+ ":" + password;
 		@SuppressWarnings("restriction")
 		String encoded = new sun.misc.BASE64Encoder().encode (loginPassword.getBytes());
-		URLConnection conn = url.openConnection();
+		URLConnection conn = urlresource.openConnection();
 		conn.setRequestProperty ("Authorization", "Basic " + encoded);
 	    InputStream input = conn.getInputStream();
 	    Reader reader = new InputStreamReader(input,"UTF-8");
