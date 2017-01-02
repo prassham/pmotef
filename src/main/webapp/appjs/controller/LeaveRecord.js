d4ba1d20584b4b75856e2cd5da388edbbae3d3f2
Prashant Sharma <prassham@in.ibm.com>
2017-01-02 12:43:59 +0530

    Changes related to TeamLeaves, Leaverecord and logout button

--- src/main/webapp/appjs/controller/LeaveRecord.js
+++ src/main/webapp/appjs/controller/LeaveRecord.js
@@ -1,6 +1,6 @@
 angular.module('LeaveRecordCtrl', []).controller('LeaveRecordController', function ($scope, PMOHttpService, $location){
-	$scope.empemail =PMOHttpService.intranetID.replace("\"", "").replace("\"", "");
-	$scope.name = $scope.empemail;
+	$scope.empemail =PMOHttpService.leaverecord.EMAIL;
+	$scope.name = PMOHttpService.leaverecord.NAME;
 	$scope.today = new Date();
 	$scope.record = PMOHttpService.getLeaverecord($scope.empemail).then(function (response){
 		$scope.record = response.data;
--- src/main/webapp/appjs/controller/TeamLeaves.js
+++ src/main/webapp/appjs/controller/TeamLeaves.js
@@ -1,12 +1,12 @@
 angular.module('TeamLeavesCtrl', []).controller('TeamLeavesController', function ($scope, PMOHttpService, $location){
 	$scope.user = PMOHttpService;
-	$scope.email =PMOHttpService.email;
+	$scope.email =$scope.user.intranetID.replace("\"", "").replace("\"", "");
 	 $scope.today = new Date();
 	/*$scope.name = $scope.user.name;*/
 	$scope.pendingbutton = $scope.user.pending;
 	$scope.notesid = null;
 	
-	$scope.pmNotesid = PMOHttpService.getNotesid(PMOHttpService.email).then(function (response){
+	$scope.pmNotesid = PMOHttpService.getNotesid($scope.email).then(function (response){
 		$scope.notes = response.data;
 		$('#mydiv').hide();
 	});
